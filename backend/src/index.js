973c5cd5f6949aafa71cc5867d7d508ea29f5cc0
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-05-16 18:47:12 -0300

    Introdução métodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,25 @@ const express = require("express");
 const app = express();
 
 app.get("/projects", (request, response) => {
-  return response.json({
-    message: "Hello Dev!",
-  });
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+});
+
+//Exemplo post
+
+app.post("/projects", (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4"]);
+});
+
+//Exemplo put, alteraçao de um elemento na API
+
+app.put("/projects", (request, response) => {
+  return response.json(["Projeto 5", "Projeto 2", "Projeto 3", "Projeto 4"]);
+});
+
+//Exemplo delete
+
+app.delete("/projects", (request, response) => {
+  return response.json(["Projeto 5", "Projeto 2", "Projeto 4"]);
 });
 
 app.listen(3333, () => {
