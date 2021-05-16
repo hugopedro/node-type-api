b0086ef8204992842ce568aa468b6afdef4e6646
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-05-16 20:55:16 -0300

    Manipulando os parâmetros de uma requisição em Node.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/projects", (request, response) => {
+  const { title } = request.query; //Desestruturação protege o usuário de manipular o endpoint.
+  console.log(title);
   return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 
 //Exemplo post
 
 app.post("/projects", (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4"]);
 });
 
 //Exemplo put, alteraçao de um elemento na API
 
-app.put("/projects", (request, response) => {
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params; // desestrutura pois pode passar mais de um parâmetro ao mesmo tempo
+  console.log(id);
   return response.json(["Projeto 5", "Projeto 2", "Projeto 3", "Projeto 4"]);
 });
 
