b96cb09523c8324a3c9d49c2f82f9ef9812a4dfe
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-05-21 15:14:05 -0300

    Middleware de autenticação para proteção das rotas

---
+++ api-vendas/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e349b03e44d32da6ccc2627f35f38925',
+    expiresIn: '1d',
+  },
+};
--- api-vendas/src/modules/users/services/CreateSessionsService.ts
+++ api-vendas/src/modules/users/services/CreateSessionsService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
@@ -29,9 +30,9 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
-    const token = sign({}, 'e349b03e44d32da6ccc2627f35f38925', {
+    const token = sign({}, authConfig.jwt.secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.jwt.expiresIn,
     });
 
     return {
