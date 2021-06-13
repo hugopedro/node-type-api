e394125cca5abd4a4826a78e8d973b68eb8c2a90
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-06-13 18:22:13 -0300

    Invalidando o Cache

--- api-vendas/src/modules/products/services/CreateProductService.ts
+++ api-vendas/src/modules/products/services/CreateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -13,15 +14,20 @@ class CreateProductService {
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
     const productExists = await productsRepository.findByName(name);
+
     if (productExists) {
       throw new AppError('There is already one product with this name');
     }
+
+    const redisCache = new RedisCache();
 
     const product = productsRepository.create({
       name,
       price,
       quantity,
     });
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productsRepository.save(product);
 
