1deb56d11873d54aff9f7ad9b77161e03c472904
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-06-20 17:29:42 -0300

    Testando o Serviço CreateCustomerService - Final

--- api-vendas/src/modules/customers/services/CreateCustomerService.spec.ts
+++ api-vendas/src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import CreateCustomerService from './CreateCustomerService';
 import FakeCustomersRepository from '@modules/customers/domain/repositories/fakes/FakeCustomersRepository';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateCustomer', () => {
   it('should be able to create a new Customer', async () => {
@@ -15,7 +16,20 @@ describe('CreateCustomer', () => {
     expect(customer).toHaveProperty('id');
   });
 
-  it('should be able to create two customers with the same email', () => {
-    expect(1).toBe(1);
+  it('should be able to create two customers with the same email', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+    await createCustomer.execute({
+      name: 'Hugo Pedro',
+      email: 'teste@teste.com',
+    });
+
+    expect(
+      createCustomer.execute({
+        name: 'Hugo Pedro',
+        email: 'teste@teste.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
